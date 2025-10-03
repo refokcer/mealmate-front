d38dad25df20453d8e59f2697ff48f2344d34336
refokcer <refokcer@gmail.com>
2025-10-03 18:19:05 +0300

    v

--- src/api/apiConfig.js
+++ src/api/apiConfig.js
@@ -1,4 +1,4 @@
-const DEFAULT_BASE_URL = 'http://localhost:8080';
+const DEFAULT_BASE_URL = 'https://localhost:7252';
 
 export const apiConfig = {
   baseUrl: process.env.REACT_APP_API_BASE_URL || DEFAULT_BASE_URL,
